07ab526b1be75526da017dc856de5dcfcbc7173a
Saqer <ahmadsaer95@outlook.com>
2023-05-13 02:34:27 +0200

    final commit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,8 +1,8 @@
 import { Redis } from "@upstash/redis"
 
 export const db = new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+    url: 'https://eu2-actual-moose-31629.upstash.io',
+    token: 'AXuNASQgMWI2NzdlM2MtMTBmMy00YWE3LTkzMDQtNjZjNDFjNWI0NGU0OWY4OWZjMThiZjkzNGM4NzkwYzFlNWRmNWU0MmYyODc=',
 
 
 })
